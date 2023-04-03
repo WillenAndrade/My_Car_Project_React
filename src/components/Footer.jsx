b3edf0057fd28983a42e2f385af34c3260cca022
Willen Andrade <71357915+WillenAndrade@users.noreply.github.com>
2023-04-03 15:59:05 -0700

    Code indentation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,25 +5,19 @@ import {BsLinkedin} from 'react-icons/bs'
 
 const Footer = () => {
     return(
-        <footer className='footer'>
-
-<div className="footer-container">
-                <div className="linkedin">
-                    <a href="https://www.linkedin.com/in/willenandrade/" target='_blank'><BsLinkedin size={32}/></a>
-                </div>
-                <div className="github">
-                <a href="https://github.com/WillenAndrade" target='_blank'><BsGithub size={32}/></a>
-                </div>
-
-
-
+      <footer className='footer'>
+          <div className="footer-container">
+            <div className="linkedin">
+                <a href="https://www.linkedin.com/in/willenandrade/" target='_blank'><BsLinkedin size={32}/></a>
             </div>
-
-            <div className="footer-top">
+            <div className="github">
+              <a href="https://github.com/WillenAndrade" target='_blank'><BsGithub size={32}/></a>
+            </div>
+         </div>
+         <div className="footer-top">
             <h2>Site criado por Willen Andrade &copy;</h2>
-            </div>
-            
-        </footer>
+         </div>
+      </footer>
     )
 }
 
