ddfbc87cb9e90b2b6db2d1bcef3826d9b60d5a56
Willen Andrade <71357915+WillenAndrade@users.noreply.github.com>
2023-04-03 15:47:53 -0700

    Optimizing footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,28 @@
 import '../components/Footer.css'
 
+import {BsGithub} from 'react-icons/bs'
+import {BsLinkedin} from 'react-icons/bs'
+
 const Footer = () => {
     return(
         <footer className='footer'>
-            <h2>Site criado por <span><a href='https://github.com/WillenAndrade ' target='_blank'>Willen Andrade &copy;</a> </span></h2>
+
+<div className="footer-container">
+                <div className="linkedin">
+                    <a href="https://www.linkedin.com/in/willenandrade/" target='_blank'><BsLinkedin size={32}/></a>
+                </div>
+                <div className="github">
+                <a href="https://github.com/WillenAndrade" target='_blank'><BsGithub size={32}/></a>
+                </div>
+
+
+
+            </div>
+
+            <div className="footer-top">
+            <h2>Site criado por Willen Andrade &copy;</h2>
+            </div>
+            
         </footer>
     )
 }
